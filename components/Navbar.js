7ee0cef5bab0f11acc8ba12914272ecf2ac9e1fe
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2022-10-10 16:33:15 +0800

    Creates landing page

---
+++ components/Navbar.js
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => (
+  <header>
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex-1 md:flex md:items-center md:gap-12">
+          <Link className="block text-green-600" href="/">
+            <a>
+              <Image
+                src="/assets/logo.svg"
+                alt="Herb"
+                width="80%"
+                height="80%"
+              />
+            </a>
+          </Link>
+        </div>
+
+        <div className="md:flex md:items-center md:gap-12">
+          <nav className="hidden md:block" aria-labelledby="header-navigation">
+            <ul className="flex items-center gap-6 text-sm">
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  About
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  🔍 &nbsp;Browse All Farm NFTs
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <button className="rounded-md bg-green-600 px-5 py-2.5 text-sm font-medium text-white shadow">
+                <Link href="/">Connect Wallet</Link>
+              </button>
+            </div>
+
+            <div className="block md:hidden">
+              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+);
+export { Navbar };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { Hero } from '../components/Hero';
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Plyant</title>
         <meta
@@ -12,8 +13,8 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}></main>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
