5ba146cfbafdd15ef5f395549569e118cbd9d6b8
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2022-10-25 17:37:56 +0800

    Adds farmer dashboard

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import ConnectWallet from './Buttons/ConnectWallet';
+import { useEffect, useState } from 'react';
+import { USER_ROLES } from '../constants';
+import { isAuthenticated } from '../services/authService';
 
 export default function Navbar() {
+  const [user, setUser] = useState(null);
+  const { farmer } = USER_ROLES;
+
+  useEffect(() => {
+    const user = isAuthenticated();
+    setUser(user);
+  }, []);
+
   return (
     <header>
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
@@ -21,6 +32,28 @@ export default function Navbar() {
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
+            <nav
+              className="hidden md:block"
+              aria-labelledby="header-navigation"
+            >
+              <ul className="flex items-center gap-6 text-sm text-gray-600">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                {user?.role === farmer && (
+                  <>
+                    <li>
+                      <Link href="/farmer/register/farm">Register Farm</Link>
+                    </li>
+                    {user?.farms && (
+                      <li>
+                        <Link href="/farmer/register/crops">Log Crops</Link>
+                      </li>
+                    )}
+                  </>
+                )}
+              </ul>
+            </nav>
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
                 <ConnectWallet />
--- pages/farmer/index.js
+++ pages/farmer/index.js
@@ -1,31 +1,36 @@
 import Layout from '../../components/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { isAuthenticated } from '../../services/authService';
 import { USER_ROLES } from '../../constants';
+import Farms from '../../components/Farms';
+import Crops from '../../components/Crops';
 
 export default function Dashboard() {
   const router = useRouter();
-  const user = isAuthenticated();
-  const { walletAddress, role, farms } = user;
-  const { farmer, distributor } = USER_ROLES;
+  const [user, setUser] = useState(null);
+  const { distributor } = USER_ROLES;
 
   useEffect(() => {
+    const user = isAuthenticated();
+    const { walletAddress, role, farms } = user;
+    setUser(user);
+
     if (!walletAddress) router.push('/');
     if (role === distributor) router.push('/distributor');
     if (!farms) router.push('/farmer/register/farm');
-  }, [distributor, farmer, role, farms, router, walletAddress]);
+  }, []);
 
   return (
     <Layout>
       <div className="p-6 flex items-center justify-center">
         <div className="container max-w-screen-lg mx-auto">
-          <div>
-            <h2 className="font-semibold text-xl text-gray-600">
-              Farmer Dashboard
-            </h2>
-            <p className="text-gray-500 mb-6">Welcome to your dashboard.</p>
-          </div>
+          <h2 className="font-semibold text-xl text-gray-600">
+            Farmer Dashboard
+          </h2>
+          <p className="text-gray-500 mb-6">Welcome to your dashboard.</p>
+          {user?.farms && <Farms farms={user.farms} />}
+          {user?.crops && <Crops crops={user.crops} />}
         </div>
       </div>
     </Layout>
